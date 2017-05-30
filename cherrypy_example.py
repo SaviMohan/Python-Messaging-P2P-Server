c48b9ce80d7aafab513abdebd27fc313ca4d69a7
smoh944 <smoh944@xen-rlimage.ece.auckland.ac.nz>
2017-05-31 11:36:33 +1200

    added password hashing

--- cherrypy_example.py
+++ cherrypy_example.py
@@ -13,100 +13,104 @@
 # The address we listen for connections on
 listen_ip = "0.0.0.0"
 listen_port = 1234
-
+salt = "COMPSYS302-2017"
 import cherrypy
+import hashlib
 
 class MainApp(object):
 
     #CherryPy Configuration
-    _cp_config = {'tools.encode.on': True, 
+	_cp_config = {'tools.encode.on': True, 
                   'tools.encode.encoding': 'utf-8',
                   'tools.sessions.on' : 'True',
                  }                 
 
-    # If they try somewhere we don't know, catch it here and send them to the right place.
-    @cherrypy.expose
-    def default(self, *args, **kwargs):
-        """The default page, given when we don't recognise where the request is for."""
-        Page = "I don't know where you're trying to go, so have a 404 Error."
-        cherrypy.response.status = 404
-        return Page
+	# If they try somewhere we don't know, catch it here and send them to the right place.
+	@cherrypy.expose
+	def default(self, *args, **kwargs):
+		"""The default page, given when we don't recognise where the request is for."""
+		Page = "I don't know where you're trying to go, so have a 404 Error."
+		cherrypy.response.status = 404
+		return Page
 
-    # PAGES (which return HTML that can be viewed in browser)
-    @cherrypy.expose
-    def index(self):
-        Page = "Welcome! This is a test website for COMPSYS302!<br/>"
+	# PAGES (which return HTML that can be viewed in browser)
+	@cherrypy.expose
+	def index(self):
+		Page = "Welcome! This is a test website for COMPSYS302!<br/>"
+		
+		try:
+		    Page += "Hello " + cherrypy.session['username'] + "!<br/>"
+		    Page += "Here is some bonus text because you've logged in!"
+		except KeyError: #There is no username
+		    
+		    Page += "Click here to <a href='login'>login</a>."
+		return Page
         
-        try:
-            Page += "Hello " + cherrypy.session['username'] + "!<br/>"
-            Page += "Here is some bonus text because you've logged in!"
-        except KeyError: #There is no username
-            
-            Page += "Click here to <a href='login'>login</a>."
-        return Page
-        
-    @cherrypy.expose
-    def login(self):
-        Page = '<form action="/signin" method="post" enctype="multipart/form-data">'
-        Page += 'Username: <input type="text" name="username"/><br/>'
-        Page += 'Password: <input type="text" name="password"/>'
-        Page += '<input type="submit" value="Login"/></form>'
-        return Page
+	@cherrypy.expose
+	def login(self):
+		Page = '<form action="/signin" method="post" enctype="multipart/form-data">'
+		Page += 'Username: <input type="text" name="username"/><br/>'
+		Page += 'Password: <input type="text" name="password"/>'
+		Page += '<input type="submit" value="Login"/></form>'
+		return Page
     
-    @cherrypy.expose    
-    def sum(self, a=0, b=0): #All inputs are strings by default
-        output = int(a)+int(b)
-        return str(output)
+	@cherrypy.expose    
+	def sum(self, a=0, b=0): #All inputs are strings by default
+		output = int(a)+int(b)
+		return str(output)
         
-    # LOGGING IN AND OUT
-    @cherrypy.expose
-    def signin(self, username=None, password=None):
-        """Check their name and password and send them either to the main page, or back to the main login screen."""
-        error = self.authoriseUserLogin(username,password)
-        if (error == 0):
-            cherrypy.session['username'] = username;
-            raise cherrypy.HTTPRedirect('/')
-        else:
-            raise cherrypy.HTTPRedirect('/login')
+	# LOGGING IN AND OUT
+	@cherrypy.expose
+	def signin(self, username=None, password=None):
+		"""Check their name and password and send them either to the main page, or back to the 			main login screen."""
+		passwordPlusSalt = password + salt
+		hashOfPasswordPlusSalt = hashlib.sha256(passwordPlusSalt).hexdigest()
+		print hashOfPasswordPlusSalt
+		error = self.authoriseUserLogin(username,password)
+		if (error == 0):
+		    cherrypy.session['username'] = username;
+		    raise cherrypy.HTTPRedirect('/')
+		else:
+		    raise cherrypy.HTTPRedirect('/login')
 
-    @cherrypy.expose
-    def signout(self):
-        """Logs the current user out, expires their session"""
-        username = cherrypy.session.get('username')
-        if (username == None):
-            pass
-        else:
-            cherrypy.lib.sessions.expire()
-        raise cherrypy.HTTPRedirect('/')
+	@cherrypy.expose
+	def signout(self):
+		"""Logs the current user out, expires their session"""
+		username = cherrypy.session.get('username')
+		if (username == None):
+		    pass
+		else:
+		    cherrypy.lib.sessions.expire()
+		raise cherrypy.HTTPRedirect('/')
         
-    def authoriseUserLogin(self, username, password):
-        print username
-        print password
-        if (username.lower() == "andrew") and (password.lower() == "password"):
-            return 0
-        else:
-            return 1
+	def authoriseUserLogin(self, username, password):
+		print username
+		print password
+		if (username.lower() == "andrew") and (password.lower() == "password"):
+		    return 0
+		else:
+		    return 1
           
 def runMainApp():
-    # Create an instance of MainApp and tell Cherrypy to send all requests under / to it. (ie all of them)
-    cherrypy.tree.mount(MainApp(), "/")
+    # Create an instance of MainApp and tell Cherrypy to send all requests under / to it. (ie all 		of them)
+	cherrypy.tree.mount(MainApp(), "/")
 
-    # Tell Cherrypy to listen for connections on the configured address and port.
-    cherrypy.config.update({'server.socket_host': listen_ip,
-                            'server.socket_port': listen_port,
-                            'engine.autoreload.on': True,
-                           })
+	# Tell Cherrypy to listen for connections on the configured address and port.
+	cherrypy.config.update({'server.socket_host': listen_ip,
+		                    'server.socket_port': listen_port,
+		                    'engine.autoreload.on': True,
+		                   })
 
-    print "========================="
-    print "University of Auckland"
-    print "COMPSYS302 - Software Design Application"
-    print "========================================"                       
-    
-    # Start the web server
-    cherrypy.engine.start()
+	print "========================="
+	print "University of Auckland"
+	print "COMPSYS302 - Software Design Application"
+	print "========================================"                       
 
-    # And stop doing anything else. Let the web server take over.
-    cherrypy.engine.block()
+	# Start the web server
+	cherrypy.engine.start()
+
+	# And stop doing anything else. Let the web server take over.
+	cherrypy.engine.block()
  
 #Run the function to start everything
 runMainApp()
